79e15f22666eb9331892f09d7319b74ec5bc1a0e
Arul <hariarul2002@gmail.com>
2024-10-21 22:21:29 +0530

    secure

--- src/Components/utils/constants.js
+++ src/Components/utils/constants.js
@@ -2,7 +2,6 @@ import vidtube from "../assets/images/youtube.png";
 
 export const APP_LOGO = vidtube;
 
-export const YOU_API = `${process.env.REACT_APP_YOU_API_KEY}`;
 export const YOUTUBE_VIDEO_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
-  YOU_API;
+  `${process.env.REACT_APP_YOU_API_KEY}`;
