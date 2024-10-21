563574d4397ba380f62607e1f181ed9718a7a569
Arul <hariarul2002@gmail.com>
2024-10-21 12:34:40 +0530

    update

--- src/Components/VideoContainer.js
+++ src/Components/VideoContainer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { YOUTUBE_VIDEO_API } from "./utils/constants";
 
 const VideoContainer = () => {
