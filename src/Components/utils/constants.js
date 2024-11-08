fa653abce3c2d69cc7032d4ec6931c779970408a
Arul <hariarul2002@gmail.com>
2024-11-08 18:30:52 +0530

    securityUpdate

--- src/Components/utils/constants.js
+++ src/Components/utils/constants.js
@@ -2,13 +2,9 @@ import vidtube from "../assets/images/youtube.png";
 
 export const APP_LOGO = vidtube;
 
-export const YOUTUBE_VIDEO_API =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
-  `${process.env.REACT_APP_YOU_API_KEY}`;
+export const YOUTUBE_VIDEO_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${process.env.REACT_APP_YOU_API_KEY}`;
 
 export const YOUTUBE_SEARCH_API =
   "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
-export const YOUTUBE_SEARCH_QUERY_API =
-  "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&key=" +
-  `${process.env.REACT_APP_YOU_API_KEY}&q=`;
+export const YOUTUBE_SEARCH_QUERY_API = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&key=${process.env.REACT_APP_YOU_API_KEY}&q=`;
